a635e3c0f90f2c564aac34b81939e58290e459ef
hyungmin19 <spartasparta819@gmail.com>
2021-09-02 17:14:50 +0900

    Spring 개인프로젝트

---
+++ src/test/java/com/example/restaurant_naverapi/RestaurantNaverapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_naverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantNaverapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
